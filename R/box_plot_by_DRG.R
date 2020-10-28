c11d1cfea6cd1a786328edf1784707039f8caeb3
Yujie Hai <yuh4005@med.cornell.edu>
2020-10-29 07:39:35 +0800

    Add files

--- R/box_plot_by_DRG.R
+++ R/box_plot_by_DRG.R
@@ -17,7 +17,7 @@ box_plot_by_DRG <-
       ## delete the legend
       theme(legend.position = 'none') +
       ## change the angle of text
-      theme(axis.text.x = element_text(angle = -45))+
+      theme(axis.text.x = element_text(angle = 90))+
       ## set label for x-axis
       xlab("DRG Code") +
       ## set label for y-axis
